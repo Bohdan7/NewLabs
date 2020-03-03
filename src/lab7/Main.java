f526a39239bf776ce5dc99bec38fe1eb02e21b20
Bohdan7 <bohdanleskovets@gmail.com>
2020-03-03 09:52:57 +0200

    initial commit

--- src/lab7/Main.java
+++ src/lab7/Main.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
+
+
+
         Main myMain = new Main();
         System.out.println(myMain.toString());
         MySet mySet = new MySet();
